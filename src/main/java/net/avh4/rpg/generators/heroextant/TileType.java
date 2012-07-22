0ec6d04cbe08d65f9ce86f67df49e4b22d25075d
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-07-22 13:29:22 -0700

    Clean up warnings.

--- src/main/java/net/avh4/rpg/generators/heroextant/TileType.java
+++ src/main/java/net/avh4/rpg/generators/heroextant/TileType.java
@@ -1,5 +1,5 @@
 package net.avh4.rpg.generators.heroextant;
 
 public enum TileType {
-	Undefined, Sea, Grassland, Forest, Jungle, Desert, GreenMountain, BarrenMountain, Frozen, River;
+    Undefined, Sea, Grassland, Forest, Jungle, Desert, GreenMountain, BarrenMountain, Frozen, River
 }
